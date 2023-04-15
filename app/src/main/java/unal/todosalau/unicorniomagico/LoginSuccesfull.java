95b98c1c2b7ab90bdc8aa4ad6ea9c6aee359b5b8
diego-debian <profediegoparra01@gmail.com>
2023-04-15 00:02:14 -0500

    Proyecto Unicornio Mágico (Implementación de authentication en Firebase)

---
+++ app/src/main/java/unal/todosalau/unicorniomagico/LoginSuccesfull.java
@@ -0,0 +1,14 @@
+package unal.todosalau.unicorniomagico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginSuccesfull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_succesfull);
+    }
+}
